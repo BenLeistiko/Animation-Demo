cb79fdb7e56b3b47f50a4a82f6f2db28c080c7d7
jamador118 <jamador118@PR22.hhs.win.fuhsd.org>
2018-05-01 11:35:30 -0700

    Time is whack

--- AnimationDemo/src/Mario.java
+++ AnimationDemo/src/Mario.java
@@ -9,11 +9,11 @@ public class Mario extends Sprite {
 
 	public static final int MARIO_WIDTH = 40;
 	public static final int MARIO_HEIGHT = 60;
-	public static final double GRAVITY = 5;
+	public static final double GRAVITY = 2;
 
 	private double velocityX,velocityY;
-	
-	
+
+
 	public Mario(PImage img, int x, int y) {
 		super(img, x, y, MARIO_WIDTH, MARIO_HEIGHT);
 		velocityX = 0;
@@ -28,12 +28,20 @@ public class Mario extends Sprite {
 
 	public void jump() {
 		// JUMP!
-		
-		
+		velocityY = -10;
+
 	}
 
 	public void act(ArrayList<Shape> obstacles) {
 		// FALL (and stop when a platform is hit)
+		y+=velocityY;
+
+		if(velocityY != 0)
+			velocityY-=GRAVITY;
+
+
+
+
 	}
 
 
