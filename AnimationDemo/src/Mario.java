b8f5ab75c0d7139dbf2457a55f6822db2158af82
jamador118 <jamador118@PR22.hhs.win.fuhsd.org>
2018-05-01 11:30:37 -0700

    VELOCITY

--- AnimationDemo/src/Mario.java
+++ AnimationDemo/src/Mario.java
@@ -9,18 +9,27 @@ public class Mario extends Sprite {
 
 	public static final int MARIO_WIDTH = 40;
 	public static final int MARIO_HEIGHT = 60;
+	public static final double GRAVITY = 5;
 
+	private double velocityX,velocityY;
+	
+	
 	public Mario(PImage img, int x, int y) {
 		super(img, x, y, MARIO_WIDTH, MARIO_HEIGHT);
+		velocityX = 0;
+		velocityY = 0;
 	}
 
 	// METHODS
 	public void walk(int dir) {
 		// WALK!
+		x+=3*dir;
 	}
 
 	public void jump() {
 		// JUMP!
+		
+		
 	}
 
 	public void act(ArrayList<Shape> obstacles) {
